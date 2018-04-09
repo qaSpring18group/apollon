e0d247665248d5dbd3ea3e4d8198d7688ec25c3a
dikobrazz <lanskix.mariya@gmail.com>
2018-04-09 20:18:35 +0300

    reload

--- apollonLaunch/apollonLaunch/Site/MainPage.cs
+++ apollonLaunch/apollonLaunch/Site/MainPage.cs
@@ -8,16 +8,16 @@ namespace apollonLaunch
 {
     public class MainPage
     {
-        string headerResourses = "//*[@id='mainmenu']/ul/li[1]/a";       //  //a[contains(.,'Разделы')] - находит 2 елемента
-        string headerGoeTag = "//*[@id='geotarget_top_selector']"; 
-        string headerEmail = "//*[@id='mainmenu']/ul/li[3]/a";           // //a[contains(.,'Почта')] - находит 2 елемеента
-        string headerFinance = "//*[@id='mainmenu']/ul/li[4]/a[1]";
-        string headerFinanceKurs = "//*[@id='mainmenu']/ul/li[4]/a[2]";
-        string headerAfisha = "//*[@id='mainmenu']/ul/li[5]/a";
-        string headerWork = "//*[@id='mainmenu']/ul/li[6]/a";
-        string headerWeather = "//*[@id='mainmenu']/ul/li[7]/a";
-        string headerTVset = "//*[@id='mainmenu']/ul/li[8]/a";
-        string headerShops = "//*[@id='mainmenu']/ul/li[9]/a";
-        string headerAutorize = "//*[@id='authorize']/div/a";
+        internal string headerResourses = "//*[@id='mainmenu']/ul/li[1]/a";       //  //a[contains(.,'Разделы')] - находит 2 елемента
+        internal string headerGoeTag = "//*[@id='geotarget_top_selector']";
+        internal string headerEmail = "//*[@id='mainmenu']/ul/li[3]/a";           // //a[contains(.,'Почта')] - находит 2 елемеента
+        internal string headerFinance = "//*[@id='mainmenu']/ul/li[4]/a[1]";
+        internal string headerFinanceKurs = "//*[@id='mainmenu']/ul/li[4]/a[2]";
+        internal string headerAfisha = "//*[@id='mainmenu']/ul/li[5]/a";
+        internal string headerWork = "//*[@id='mainmenu']/ul/li[6]/a";
+        internal string headerWeather = "//*[@id='mainmenu']/ul/li[7]/a";
+        internal string headerTVset = "//*[@id='mainmenu']/ul/li[8]/a";
+        internal string headerShops = "//*[@id='mainmenu']/ul/li[9]/a";
+        internal string headerAutorize = "//*[@id='authorize']/div/a";
     }
 }
