98f47b62518b5045be693baf74c892c4a23aeba3
maria2709 <maryiagaydayenko@gmail.com>
2018-04-13 01:33:20 +0300

    FT_6M edited

---
+++ apollonLaunch/apollonLaunch/Site/Rebenok_byPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace apollonLaunch.Site
+{
+    class Rebenok
+    {
+        internal string XpathRebenok = "//a[contains(text(),'Rebenok.by')]";
+      
+    }
+}
--- apollonLaunch/apollonLaunch/Tests/Gaydayenko/FT_6M.cs
+++ apollonLaunch/apollonLaunch/Tests/Gaydayenko/FT_6M.cs
@@ -6,39 +6,42 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System.Collections.Generic;
 using apollonLaunch;
-
+using apollonLaunch.Site;
 
 namespace apollonLaunch
 
 {
     [TestClass]
-    public class FT_6M_Gaydayenko
+    public class FT_6M
 
     {
         //private const string XpathMainMenu = "//*[@id='mainmenu']/ul/li[1]/a[contains (text(), 'Разделы')]";
-        private const string XpathToSite = "//a[contains(text(),'Rebenok.by')]";
+        //private const string XpathToSite = "//a[contains(text(),'Rebenok.by')]";
 
         [TestMethod]
 
-        public void TestMethod1()
+        public void FT_6M_test()
         {
             IWebDriver browser;
             MainPage mainPage = new MainPage();
-
+            Rebenok rebenok_byPage = new Rebenok();
             browser = new OpenQA.Selenium.Chrome.ChromeDriver();
             browser.Manage().Window.Maximize();
 
-            browser.Url= "https://tut.by";
+            //browser.Url= "https://tut.by";
+            browser.Url = UsefullMethods.OpenTytbySite;
 
             IWebElement searchLink = browser.FindElement(By.XPath(mainPage.headerResourses));
 
             searchLink.Click();
 
-            IWebElement searchLinkToSite = browser.FindElement(By.XPath(XpathToSite));
+            IWebElement searchLinkToSite = browser.FindElement(By.XPath(rebenok_byPage.XpathRebenok));
             searchLinkToSite.Click();
 
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(browser.FindElement(By.XPath(XpathToSite)).Displayed);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(browser.FindElement(By.XPath(rebenok_byPage.XpathRebenok)).Displayed);
 
+            browser.SwitchTo().Window(browser.WindowHandles[0]);
+            browser.FindElement(By.XPath(mainPage.CloseheaderResourses)).Click();
             browser.Quit();
         }
 
